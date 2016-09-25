820126f3e62d3707caad86f4638d51300c37a5e1
Cassidimus <cassiehinkson@msn.com>
2016-09-25 19:46:06 +0100

    added comments

--- campervan.m
+++ campervan.m
@@ -4,12 +4,12 @@ numberOfPDE = 1;
 pdem = createpde(numberOfPDE);
 
 %create geometry
-P1 = [2;6;4.1;4.5;4.5;3.6;3.6;4.1;0.65;0.65;0.9;0.9;0.87;0.87];
-C1 = [3;4;1;5;5;1;0;0;2.5;2.5];
+P1 = [2;6;4.1;4.5;4.5;3.6;3.6;4.1;0.65;0.65;0.9;0.9;0.87;0.87]; %pot
+C1 = [3;4;1;5;5;1;0;0;2.5;2.5]; %campervan
 C1 = [C1;zeros(length(P1)-length(C1),1)];
 T1 = [3;4;4;5;5;4;0;0;0.5;0.5];
-T1 = [T1;zeros(length(P1)-length(T1),1)];
-H1 = [4;4.3;0.575;0.05;0.07;0];
+T1 = [T1;zeros(length(P1)-length(T1),1)]; %table
+H1 = [4;4.3;0.575;0.05;0.07;0]; %heat source
 H1 = [H1;zeros(length(P1)-length(H1),1)];
 
 gd=[P1,C1,T1,H1]; %includes goemetries
